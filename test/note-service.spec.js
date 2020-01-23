97c6a6974cba41877157dc4c2fe3c2cdeb6ce3e5
Dakota Heim <kotieheim@gmail.com>
2020-01-23 16:27:50 -0700

    remove logs

--- test/note-service.spec.js
+++ test/note-service.spec.js
@@ -1,64 +1,63 @@
-'use strict';
-require('dotenv').config();
-const knex = require('knex');
+"use strict";
+require("dotenv").config();
+const knex = require("knex");
 
-const NoteService = require('../src/note-service');
+const NoteService = require("../src/note-service");
 
-describe.only('Note service object', function() {
+describe.only("Note service object", function() {
   let db;
 
   let testNotes = [
     {
-      note_name: 'Important',
+      note_name: "Important",
       id: 1,
       folder_id: 1
     },
     {
-      note_name: 'Super',
+      note_name: "Super",
       id: 2,
       folder_id: 1
     },
     {
-      note_name: 'Spangley',
+      note_name: "Spangley",
       id: 3,
       folder_id: 1
     }
   ];
 
-  let newNote = [{ note_name: 'Grocery', id: 4, folder_id: 2 }];
+  let newNote = [{ note_name: "Grocery", id: 4, folder_id: 2 }];
 
-  before('Get database instance', () => {
+  before("Get database instance", () => {
     db = knex({
-      client: 'pg',
+      client: "pg",
       connection: process.env.TEST_DB_URL
     });
   });
 
-  after('Close database', () => {
+  after("Close database", () => {
     db.destroy();
   });
 
-  beforeEach('Reset the test database', () => {
-    return db('note').truncate();
+  beforeEach("Reset the test database", () => {
+    return db("note").truncate();
   });
 
-  beforeEach('Insert test data into note table', () => {
-    return db.into('note').insert(testNotes);
+  beforeEach("Insert test data into note table", () => {
+    return db.into("note").insert(testNotes);
   });
 
-  describe('getAllNotes', () => {
-    it('it returns all notes from notes table', () => {
+  describe("getAllNotes", () => {
+    it("it returns all notes from notes table", () => {
       return NoteService.getAllNotes(db).then(notes => {
         expect(notes).to.eql(testNotes);
       });
     });
   });
 
-  describe.only('addNote', () => {
-    it('it should add a folder to the folder table', () => {
+  describe.only("addNote", () => {
+    it("it should add a folder to the folder table", () => {
       return NoteService.addFolder(db, newNote).then(note => {
         expect(note).to.eql(newNote);
-        console.log(note, newNote);
       });
     });
   });
